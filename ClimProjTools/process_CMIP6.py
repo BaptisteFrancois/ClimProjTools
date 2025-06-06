d2d24cdeb3133660a6ace0533c647b9bdbf6a1a2
BaptisteFrancois <baptistefrancois51@gmail.com>
2025-06-06 12:56:47 -0400

    remove hard coded value in process_CMIP6.py

--- ClimProjTools/process_CMIP6.py
+++ ClimProjTools/process_CMIP6.py
@@ -185,8 +185,8 @@ def extract_basin_average_from_CMIP6_data(variables, experiments, basin_shapefil
                 weights = pd.DataFrame(columns=['lat', 'lon', 'weight'])
                 for k, grid in enumerate(grid_cells.geometry):
 
-                    grid = gpd.GeoDataFrame({'geometry': [grid]})
-                    grid.crs = 'EPSG:2317'
+                    grid = gpd.GeoDataFrame({'geometry': [grid]}, epsg=epsg)
+                    
                     intersections = gpd.overlay(grid, basin, how='intersection')
 
                     # If the intersection is empty, the fraction is 0
